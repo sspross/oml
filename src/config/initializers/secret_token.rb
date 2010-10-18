dfb245fe2d611bc73d39f4e78bf9bf7a4712dd1d
Silvan Spross <silvan.spross@gmail.com>
2010-10-18 16:25:06 +0200

    tried upgrading to rails3, but a lot of plugins doesn't exist. maybe dead-end

---
+++ src/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Src::Application.config.secret_token = '740fa6a2a2ab938f13b41d85f854d60ab2ba1e8019b63dbaedfbdaadd594f530af378b538f48ad874daf31252d1e9799eb59b3f79fd1509bab0ab5c9f0149fc6'
--- src/config/initializers/session_store.rb
+++ src/config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_oml_session',
-  :secret      => 'c4972a450a8fd77a8028f9c1a611c6a07678e68fb42955a093ab1b5a937d2c76bdc233fd4608a15d8e216ff260b10e38401d7f7bcfdb2fae6f44ce7df5629099'
-}
+Src::Application.config.session_store :cookie_store, :key => '_src_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Src::Application.config.session_store :active_record_store
