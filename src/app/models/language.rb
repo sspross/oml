5c3fc3518f1e944a88c9a01fbcd1989febf9e4ca
Silvan Spross <silvan.spross@gmail.com>
2010-11-15 20:28:08 +0100

    deleted deadended rails updating

--- src/app/models/language.rb
+++
@@ -1,7 +0,0 @@
-class Language < ActiveRecord::Base
-  
-  translates :name
-  
-  validates_presence_of :name
-  
-end
--- src/app/models/title.rb
+++
@@ -1,7 +0,0 @@
-class Title < ActiveRecord::Base
-  
-  belongs_to :language
-  
-  validates_presence_of :name, :language
-  
-end
