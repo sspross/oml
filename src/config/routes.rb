1f7078860b43c951b3f294ee6bda941e68311c1d
Silvan Spross <silvan.spross@gmail.com>
2010-11-16 16:22:34 +0100

    added json and xml respond_to and langauge in path

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,12 +1,17 @@
 ProjectZero::Application.routes.draw do
+
   root :to => "home#index"
+
+  scope "/:locale" do
+    
+    resources :movies
+    resources :users
+    resources :user_sessions
+    
+    match '/login', :to => 'user_sessions#new', :as => :login
+    match '/logout', :to => 'user_sessions#destroy', :as => :logout
+    
+    match '/exception_test' => 'exception_test#error'
+  end
   
-  resources :movies
-  resources :users
-  resources :user_sessions
-
-  match '/login', :to => 'user_sessions#new', :as => :login
-  match '/logout', :to => 'user_sessions#destroy', :as => :logout
-
-  match '/exception_test' => 'exception_test#error'
 end
