bda1cb74565bf51765da279b5005617279c3418c
Silvan Spross <silvan.spross@gmail.com>
2010-11-17 18:17:28 +0100

    changed title of app and deploying

--- src/config/deploy.rb
+++ src/config/deploy.rb
@@ -1,30 +1,25 @@
 require 'bundler/capistrano'
 
-set :application, "project_zero"
+set :application, "oml"
 
-role :app, "test.panter.ch"
-role :web, "test.panter.ch"
-role :db,  "test.panter.ch", :primary => true
-set :rails_env, 'production'
+role :app, "orwell.ch"
+role :web, "orwell.ch"
+role :db,  "orwell.ch", :primary => true
 
-set :deploy_via, :remote_cache
-set :git_enable_submodules, 1
-set :scm, :git
-set :default_run_options, { :pty => true }
-set :repository, "git@github.com:panter/project_zero.git"
-set :ssh_options, {:forward_agent => true}
-set :deploy_to, "/home/rails/app"
-set :user, "rails"
+# deploy
+set :deploy_to, "/home/sspross/projects/#{application}"
+set :user, "sspross"
 set :use_sudo, false
 
-task :update_config_links, :roles => [:app] do
-  run "ln -sf #{shared_path}/config/* #{release_path}/config/"
-end
-after "deploy:update_code", :update_config_links
+# scm
+set :scm, :git
+set :repository, "git://github.com/sspross/semesterarbeit.git"
+set :branch, "develop"
 
+# deploy task
 namespace :deploy do
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
+  desc "Tell Passenger to restart the app."
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
