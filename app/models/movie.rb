43baa3b915922e45166257ef1148e17ffa0cac4e
Silvan Spross <silvan.spross@gmail.com>
2010-11-25 19:00:32 +0100

    removed not needed and some deprecated stuff$

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,10 @@ class Movie < ActiveRecord::Base
   translates :title, :description
   validates_presence_of :title
   has_many :ratings
+  has_many :users, :through => :ratings
+  
+  def rated_by?(user)
+    self.users.include?(user)
+  end
+  
 end
