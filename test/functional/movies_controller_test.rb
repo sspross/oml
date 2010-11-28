6c1f5524069f3dcfc1d56411755eeb9cbdcc7701
Silvan Spross <silvan.spross@gmail.com>
2010-11-28 18:28:53 +0100

    added functional tests

--- test/functional/movies_controller_test.rb
+++ test/functional/movies_controller_test.rb
@@ -1,4 +1,49 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionController::TestCase
+  
+  context "GET on index" do
+    setup { get :index, :locale => I18n.locale }
+    should respond_with(:success)
+  end
+  
+  context "GET on new" do
+    setup { get :new, :locale => I18n.locale }
+    should respond_with(:success)
+  end
+  
+  context "Minimal Movie" do
+    context "POST on create with valid data" do
+      setup do
+        post :create,
+             :locale => I18n.locale,
+             :movie => Factory.attributes_for(:movie)
+      end
+      should respond_with(:redirect)
+    end
+  end
+    
+  context "Existing Movie" do
+    setup { @movie = Factory(:movie) }
+
+    context "UPDATE on post" do
+      setup do
+        post :update,
+             :id => @movie.id,
+             :movie => Factory.attributes_for(:movie),
+             :locale => I18n.locale
+      end
+      should respond_with(:redirect)
+    end
+
+    context "DELETE on destroy" do
+      setup do
+        delete :destroy, 
+               :id => @movie.id, 
+               :locale => I18n.locale
+      end
+      should respond_with(:redirect)
+    end
+  end
+  
 end
