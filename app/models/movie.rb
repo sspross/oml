5c5b463eb7c865bb98394a1585901d867831caad
Silvan Spross <silvan.spross@gmail.com>
2010-11-28 16:08:32 +0100

    updated model attributes and calculations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,19 +4,26 @@ class Movie < ActiveRecord::Base
   has_many :ratings
   has_many :users, :through => :ratings
   
-  validates_presence_of :title
-  validates_uniqueness_of :title
-  
   def rated_by?(user)
     self.users.include?(user)
   end
   
   def rating_all
-    self.ratings.sum
+    return nil if self.ratings.count < 1
+    return (self.ratings.sum(:value).to_f / self.ratings.count.to_f).to_f
   end
   
-  def rating_friends(user)
-    self.ratings.sum
+  def rating_friends_of(user)
+    sum = 0
+    count = 0
+    for friend in user.friends
+      if friend.rating_of_movie(self)
+        sum += friend.rating_of_movie(self).value
+        count += 1
+      end
+    end
+    return nil if count < 1
+    return (sum.to_f / count.to_f).to_f
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :direct_friends, :through => :friendships, :conditions => ['approved = ?', true], :source => :friend
   has_many :inverse_friends, :through => :inverse_friendships, :conditions => ['approved = ?', true], :source => :user
-  has_many :pending_friends, :through => :friendships, :conditions => ['approved = ?', false], :foreign_key => "user_id", :source => :user
+  has_many :pending_friends, :through => :friendships, :conditions => ['approved = ?', false], :foreign_key => "user_id", :source => :friend
+  has_many :requesting_friends, :through => :requested_friendships, :source => :user
   has_many :requested_friendships, :class_name => "Friendship", :foreign_key => "friend_id", :conditions => ['approved = ?', false]
  
   def friends
     direct_friends | inverse_friends
+  end
+  
+  def rating_of_movie(movie)
+    self.ratings(:conditions => ['movie_id = ?', movie.id]).first
   end
 end
