3b6b12e52b7ea86fd6c03d482cfed4b37e31e329
Silvan Spross <silvan.spross@gmail.com>
2010-11-28 18:32:30 +0100

    added rating over all value to movie and movies xml and json respond

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,10 @@
 class MoviesController < ApplicationController
   resource_controller
+
+  index.wants.xml { render :xml => @movies.to_xml(:methods => [:rating_all]) }
+  index.wants.json { render :json => @movies.to_json(:methods => [:rating_all]) }
+  
+  show.wants.xml { render :xml => @movie.to_xml(:methods => [:rating_all]) }
+  show.wants.json { render :json => @movie.to_json(:methods => [:rating_all]) }
+  
 end
