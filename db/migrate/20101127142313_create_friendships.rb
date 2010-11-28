c537a3c61d3574aaf03d398a4ad01493fb98545c
Silvan Spross <silvan.spross@gmail.com>
2010-11-28 16:08:04 +0100

    added default value to approved flag in friendship

--- db/migrate/20101127142313_create_friendships.rb
+++ db/migrate/20101127142313_create_friendships.rb
@@ -3,7 +3,7 @@ class CreateFriendships < ActiveRecord::Migration
     create_table :friendships do |t|
       t.integer :user_id
       t.integer :friend_id
-      t.boolean :approved
+      t.boolean :approved, :default => false
 
       t.timestamps
     end
